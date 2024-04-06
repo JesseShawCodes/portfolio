d955b44e0d3d674ffc30272b82ef9b68ced8f97b
Jesse Shaw <JesseShawCodes@Gmail.com>
2024-04-06 09:02:55 -0400

    removed unused icons

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import { faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export default function Landing(props) {
   console.log(props)
