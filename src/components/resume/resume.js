ef32aab3d43434742c3f19826f4fcb694d4fe61b
Jesse Shaw <JesseShawCodes@Gmail.com>
2024-04-08 13:04:49 -0400

    Added bootstrap, some images, data.json updates

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,18 +1,36 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 export default function Landing(props) {
-  console.log(props)
   return (
       <>
       <h1>
         Resume
       </h1>
+      <button className='download_button'>
+      Download CSV <FontAwesomeIcon icon={faDownload} />
+      </button>
       <div>
         {
           props.content.map((item) => 
             <section>
               <h2>{item.company}</h2>
+
+
               <h3>{item.title}</h3>
+              <h4>{item.start_date} - {item.end_date ? item.end_date : "Present" }</h4>
+
+              <div>
+                <ul>
+                {item.bullet_points.map((val) =>
+                  <li>{val}</li>
+                )}
+                </ul>
+              </div>
             </section>
           )
         }
--- src/components/whoiam/whoiam.js
+++ src/components/whoiam/whoiam.js
@@ -10,7 +10,7 @@ export default function Whoiam(props) {
     return (
         <section className="about-me" id="bio">
             <section className="image">
-                <img src={myPicture} alt="Picture of Jesse Shaw" className="my-picture"></img>
+                <img src={myPicture} alt="Jesse Shaw in a suit" className="my-picture"></img>
             </section>
             <section className="description">
                 <span className="aboutme-description">Hey there! My name is Jesse Shaw. I am a Full Stack Developer currently working on a Learning Management System application at Jacob's Engineering. I enjoy coding because I love to create things. I enjoy building things from scratch and seeing them come to life. I am always interested in meeting and learning from fellow developers and creatives who have similar mindsets. I love working with people who are not afraid to challenge each other and make the world a better place.</span>
