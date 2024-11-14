972a0ef0dce9d74b0ccd9e847fe137fa582ae495
Jesse Shaw <JesseShawCodes@gmail.com>
2024-11-14 13:08:26 -0500

    Updating profile content

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -3,17 +3,16 @@ import './landing.css';
 
 import Whoiam from '../whoiam/whoiam';
 
-export default function Landing() {
+export default function Landing(aboutMe) {
   return (
     <>
       <section className="landing">
         <section className="greeting-section">
           <span className="greeting">My name is Jesse. I am a Full Stack Developer</span>
         </section>
-
       </section>
       <section className="container_portfolio">
-        <Whoiam />
+        <Whoiam content={aboutMe} />
       </section>
     </>
   );
--- src/components/whoiam/whoiam.jsx
+++ src/components/whoiam/whoiam.jsx
@@ -1,8 +1,8 @@
 import { React, useEffect, useState } from 'react';
-import DOMPurify from 'dompurify';
+import PropTypes from 'prop-types';
 import myPicture from '../../images/IMG_2233.JPG';
 
-export default function Whoiam() {
+export default function Whoiam({ aboutMe }) {
   const [state, setState] = useState({ description: null });
 
   useEffect(() => {
@@ -13,16 +13,26 @@ export default function Whoiam() {
       });
   }, []);
 
-  const aboutMe = DOMPurify.sanitize(state.description, { USE_PROFILES: { html: false } });
-
   return (
     <section className="about-me" id="bio">
       <section className="image">
         <img src={myPicture} alt="Jesse Shaw in a suit" className="my-picture" title="Me on my wedding day in December 2021" />
       </section>
       <section className="description aboutme-description">
-        {aboutMe}
+        {aboutMe.content}
       </section>
     </section>
   );
 }
+
+Whoiam.defaultProps = {
+  aboutMe: {
+    content: 'Hey there! My name is Jesse Shaw. I am a Full Stack Developer currently working on a Learning Management System application at Amentum. I enjoy coding because I love to create things. I enjoy building things from scratcssh and seeing them come to life. I am always interested in meeting and learning from fellow developers and creatives who have similar mindsets. I love working with people who are not afraid to challenge each other and make the world a better place.',
+  },
+};
+
+Whoiam.propTypes = {
+  aboutMe: PropTypes.shape({
+    content: PropTypes.string.isRequired,
+  }),
+};
