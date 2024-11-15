e417616382d5ba1b0398273c5648aa4ecdd29b16
Jesse Shaw <JesseShawCodes@gmail.com>
2024-11-14 21:12:58 -0500

    About Me Content update

--- src/components/whoiam/whoiam.jsx
+++ src/components/whoiam/whoiam.jsx
@@ -1,17 +1,9 @@
-import { React, useEffect, useState } from 'react';
+import { React } from 'react';
 import PropTypes from 'prop-types';
 import myPicture from '../../images/IMG_2233.JPG';
 
 export default function Whoiam({ aboutMe }) {
-  const [state, setState] = useState({ description: null });
-
-  useEffect(() => {
-    fetch(process.env.REACT_APP_DRUPAL_ENDPOINT)
-      .then((response) => response.json())
-      .then((data) => {
-        setState({ ...state, description: data.data[0].attributes.body.value });
-      });
-  }, []);
+  const { content } = aboutMe;
 
   return (
     <section className="about-me" id="bio">
@@ -19,7 +11,7 @@ export default function Whoiam({ aboutMe }) {
         <img src={myPicture} alt="Jesse Shaw in a suit" className="my-picture" title="Me on my wedding day in December 2021" />
       </section>
       <section className="description aboutme-description">
-        {aboutMe.content}
+        {content}
       </section>
     </section>
   );
@@ -27,7 +19,7 @@ export default function Whoiam({ aboutMe }) {
 
 Whoiam.defaultProps = {
   aboutMe: {
-    content: 'Hey there! My name is Jesse Shaw. I am a Full Stack Developer currently working on a Learning Management System application at Amentum. I enjoy coding because I love to create things. I enjoy building things from scratcssh and seeing them come to life. I am always interested in meeting and learning from fellow developers and creatives who have similar mindsets. I love working with people who are not afraid to challenge each other and make the world a better place.',
+    content: 'Hi! My name is Jesse Shaw. I am a Full Stack Developer currently working on a Learning Management System application at Amentum. I enjoy coding because I love to create things. I enjoy building things from scratcssh and seeing them come to life. I am always interested in meeting and learning from fellow developers and creatives who have similar mindsets. I love working with people who are not afraid to challenge each other and make the world a better place.',
   },
 };
 
