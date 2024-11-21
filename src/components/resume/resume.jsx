38bef51cb1424e9d4433bbf473ed103ea3bba5ca
Jesse Shaw <JesseShawCodes@gmail.com>
2024-11-20 21:02:06 -0500

    Resume Update

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/self-closing-comp */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -10,7 +7,6 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import resumePdf from '../../data/JesseShaw_Resume.pdf';
-// import ResumeSection from './resumeSection';
 
 export default function Resume(resume) {
   const { resumeContent } = resume;
@@ -27,6 +23,25 @@ export default function Resume(resume) {
         </button>
       </a>
       <div style={{ maxWidth: '700px', margin: '0px auto' }}>
+        {
+          resumeContent.map((item) => (
+            <section className="card my-2 p-3" key={item.company}>
+              <h2>{item.company}</h2>
+              <h3>{item.title}</h3>
+              <h4>
+                {item.start_date}
+                -
+                {item.end_date ? item.end_date : 'Present' }
+              </h4>
+
+              <div>
+                <ul>
+                  {item.bullet_points.map((val) => <li key={crypto.randomUUID()}>{val}</li>)}
+                </ul>
+              </div>
+            </section>
+          ))
+        }
       </div>
     </div>
   );
